5312b141daafcea802a63e4f06735c4621395baf
branislav985 <branislav985@gmail.com>
2023-08-15 22:51:58 +0200

    add propreties into module.export

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
 	e2e: {
+		video: false,
+		defaultCommandTimeout: 5000,
+		pageLoadTimeout: 10000,
+		// ignoreTestFiles: ['*.js', '*.md'],
 		specPattern: './cypress/integration/*feature',
 		setupNodeEvents(on, config) {
 			on('file:preprocessor', cucumber())
